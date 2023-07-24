aab775ed321e7d0eab6fea712dd91f378a623d8f
mehedi <mehedisun@outlook.com>
2023-07-24 21:19:32 +0600

    Moved from data annotation to Fluent API

--- Models/CharacterContext.cs
+++ Models/CharacterContext.cs
@@ -10,9 +10,29 @@ namespace MovieStar.Models
 
         }
 
+
+        // Models
         public DbSet<Character> Characters => Set<Character>();
         public DbSet<Movie> Movies => Set<Movie>();
 
         public DbSet<User> Users => Set<User>();
+
+
+
+        // Model Configuration Using Fluent API
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           // User
+           builder.Entity<User>().HasKey(e=> e.Id);
+           builder.Entity<User>().Property(e=> e.FirstName).IsRequired().HasMaxLength(50);
+           builder.Entity<User>().Property(e=> e.LastName).IsRequired().HasMaxLength(50);
+           builder.Entity<User>().Property(e=> e.Email).IsRequired().HasMaxLength(150);
+            
+
+        }
+
+
+
+
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -1,23 +1,11 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MovieStar.Models
 {
     public class User: BaseModel
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-
-        [Required]
-        [StringLength(50, MinimumLength =2)]
         public string FirstName { get; set; } = string.Empty;
-        [Required]
-        [StringLength(50, MinimumLength =2)]
         public string LastName { get; set; } = string.Empty;
-
-        [Required]
-        [StringLength(120, MinimumLength =10)]
         public string Email { get; set; } = string.Empty;
         public byte[] PasswordHash { get; set; } = new byte[0];
         public byte[] PasswordSalt { get; set; } = new byte[0];
